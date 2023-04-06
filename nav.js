23a957394357ac3f0bc750583bca61b91f9091e1
Maria Magdalini <momoloca42@gmail.com>
2023-04-06 13:48:47 +0300

    minor adjustments in navbar

--- nav.js
+++ nav.js
@@ -1,9 +1,14 @@
 
 var links = document.querySelectorAll('.nav-link')
 var array = links.forEach(function(link){
-    link.addEventListener('click',()=>input.checked = false)
+    link.addEventListener('click', scrollUp)
+
 })
 
 
 
 const input = document.querySelector('input') 
+function scrollUp(){
+    input.checked = false
+    this.scrollIntoView(false)
+}
