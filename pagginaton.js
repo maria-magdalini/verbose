6cd5ba0bf109290f4edfd8bb89f48f1dfba2e632
Limit3c <momoloca42@gmail.com>
2023-04-20 12:54:02 +0300

    paggination int atv carousel

--- pagginaton.js
+++ pagginaton.js
@@ -1,4 +1,4 @@
-const paggination = document.querySelectorAll("[data-carousel-button]");
+const paggination = document.querySelectorAll("[data-moto-button]");
 
 paggination.forEach(button => {
     button.addEventListener("click", () => {
@@ -22,19 +22,25 @@ paggination.forEach(button => {
 })
 
 
-const atvButtonPrev =  document.getElementById('atvPrev');
-const atvButtonNext =  document.getElementById('atvNext');
+const atvPaggination = document.querySelectorAll("[data-atv-button]");
 
-atvButtonNext.addEventListener('click', ()=>{
- pagg()
+atvPaggination.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.atvButton === "next" ? 1 : -1;
+       
+        
+        const slides = document.querySelector("[data-paggination-slides-atv]");
+        // console.log(slides)
+
+        // console.log([...slides.children])
+        const activeSlide = slides.querySelector("[data-scale]")
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+        //console.log(newIndex)
+        if (newIndex <0 ) newIndex = slides.children.length -1;
+        if (newIndex >= slides.children.length ) newIndex = 0;
+        slides.children[newIndex].dataset.scale = true;
+        delete activeSlide.dataset.scale
+
+      
+    })
 })
-
-
-
-function pagg(){
- const imgArray = document.querySelector(".atv-pics-preview")
-console.log(imgArray[0])
- }
-
-
-// console.log(atvButtonNext, atvButtonPrev)
