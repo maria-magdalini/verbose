f4dfa1dd20d8ae067f9062e615bb6997834389d5
Maria Magdalini <momoloca42@gmail.com>
2023-05-03 13:09:04 +0300

    atvs desc change

--- index.js
+++ index.js
@@ -26,30 +26,49 @@ button.forEach(button => {
     })
 })
 
-const atvButtons =  document.querySelectorAll("[data-atv-button]");
 
+
+
+
+
+const atvButtons =  document.querySelectorAll("[data-atv-button]");
 const atvColor = document.getElementById('atvSpecsCnt').children[5];
 const atvName = document.getElementById('atvSpecsCnt').children[0];
 
-atvButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-        
-        const slides = button.closest('[data-carousel]').querySelector("[data-slides]");
 
 
+const atvSpecsChange = function(color, name, num , motoType){
+        color.innerText="Colors : "+ motoType[num].color;
+        name.innerText = motoType[num].scooteName;
+        return
+}
+
+const anon = function (){ 
+        //check if button has data-carousel-button="prev" or "next"
+        const offset = this.dataset.carouselButton === "next" ? 1 : -1; //used for getting the index of active slide latter
+        //get the slides that are closest to object with data-carousel
+        const slides = this.closest('[data-carousel]').querySelector("[data-slides]");
+        //selecting the active slide by selecting the one with the dataset of active
         const activeSlide = slides.querySelector("[data-active]")
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-        console.log(newIndex);
+        //getting the index of the active slide + the offset 
+         let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+        
+        //
         if (newIndex <0 ) newIndex = slides.children.length -1;
         if (newIndex >= slides.children.length ) newIndex = 0;
-       // console.log(slides.children[newIndex])
-        atvColor.innerText="Colors : "+ atvs[newIndex].color;
-        atvName.innerText = atvs[newIndex].scooteName;
 
-       
+        atvSpecsChange(atvColor, atvName , newIndex , atvs)
+
+}
+
+
+
+
+
+atvButtons.forEach(button => {
+        button.addEventListener("click", anon)
     })
-})
 
-console.log(atvs)
+
+
 
